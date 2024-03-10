3dcfccc41c78b0c1c23b58736f68c8ae1bc350fd
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2024-03-10 16:34:26 +1100

    added remove link animation

--- app/(dashboard)/profile/[profileId]/(forms)/profile-links/page.tsx
+++ app/(dashboard)/profile/[profileId]/(forms)/profile-links/page.tsx
@@ -1,6 +1,8 @@
 import ProfileLinksDisplay from "./components/ProfileLinksDisplay";
 
 const ProfileIdPage = async () => {
+  
+
   return (
     <>
       <ProfileLinksDisplay />
--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -20,23 +20,3 @@ export default function SetupPage() {
   return <>Profile Page</>;
 }
 
-// export default async function ProfilePage() {
-//     const { userId } = auth()
-//     const user = await currentUser()
-
-//     return (
-//         <>
-//             Profile Page
-//             <br/>
-//             {userId}
-//             <br/>
-//             currentUser: {user?.id}
-//             <br/>
-//             email: {user?.emailAddresses[0].emailAddress}
-//             <br/>
-//             first_name: {user?.firstName}
-//             <br/>
-//             last_name: {user?.lastName}
-//         </>
-//     )
-// }
